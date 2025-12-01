64cc82310d5e85628327f2f74baf980d31967351
MohamedMiladi777 <miladyshady@gmail.com>
2025-12-01 13:14:12 +0100

    added bisection technique tutorial

---
+++ Learning-Resources/README.m
@@ -0,0 +1,19 @@
+* [git - the simple guide](https://rogerdudler.github.io/git-guide/)
+
+* [git cheat sheet](https://rogerdudler.github.io/git-guide/files/git_cheat_sheet.pdf)
+
+* [Tiger Style](https://tigerstyle.dev/)
+
+* [What the heck is the event loop anyway?](https://www.youtube.com/watch?v=8aGhZQkoFbQ)
+	* Event loops beautifully explained.
+
+* [JavaScript in Plain English](https://javascript.plainenglish.io/)
+
+* [Let's build GPT: from scratch, in code, spelled out.](https://www.youtube.com/watch?v=kCc8FmEb1nY)
+
+* [Interactive Git Learning Material](https://learngitbranching.js.org/)
+
+* [Algorithms for Optimization](https://algorithmsbook.com/optimization/files/optimization.pdf)
+
+* [Basic bisection to find a bug with Git](https://www.youtube.com/watch?v=X_pQfoaRuhA)
+	* This is an important aspect to grasp for bug finding.
